bab68a307df5d59b00e0aefc642e0ee0e320608d
Mavriki <55285178+MavrikiDev@users.noreply.github.com>
2020-04-28 16:25:16 -0300

    Se agrega al repositorio
    Se agrega la primera versión funcional de la herramienta diseñada, en principio, para automatizar la descarga de libros PDF publicados GRATUITAMENTE por la editorial springer.

---
+++ newURL.py
@@ -0,0 +1,59 @@
+def leerTxt(ruta):
+	listaString = []
+	with open(ruta) as file:
+		for linea in file:
+			listaString.append(linea.split())
+	return listaString
+
+def getURL(listaString):
+	#Captura las urls de un txt y las escribe en otro
+	urls = []
+	for sublista in listaString:
+		for cadena in sublista:
+			if(cadena[0:4] == "http"):
+				urls.append(cadena)
+				print ("Se agregó la URL " + cadena)
+	return urls
+
+def getISBN(listaString):
+	#Captura el ISBN de la URL
+	isbn = []
+	for sublista in listaString:
+		for cadena in sublista:
+			if(cadena[0:4] == "http"):
+				isbn.append(cadena[-17:])
+				#obtengo ISBN de la url
+				print ("Se agregó la URL " + cadena[-17:])
+	return isbn
+
+
+
+#Escribiendo un nuevo archivo con las url...
+def escribirArchivo(isbn):
+	f=open("urls.txt","w")
+	for num in isbn:
+		f.write("https://link.springer.com/content/pdf/10.1007%2F"+num+".pdf"+"\n")
+
+	f.close()
+	print ("Archivo creado con éxito")
+
+
+print ("""
+	==============================================
+	==================DETECT URL==================
+	==============================================
+
+	
+	""")
+
+try:
+	ruta = input("Escriba la ruta del archivo:")
+	listaString = leerTxt(ruta)
+	isbn = getISBN(listaString)
+	escribirArchivo(isbn)
+except Exception as e:
+	raise e#print ("Ha ocurrido un error inesperado\n")
+finally:
+	print ("Goodbye")
+
+	
